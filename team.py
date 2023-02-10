49f9f62746aee0c3a3bcd031ca979044aace5b5d
MattByro <124500843+MattByro@users.noreply.github.com>
2023-02-10 16:58:02 +0000

    Add files via upload

---
+++ team.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 10 16:26:16 2023
+
+@author: N1163880
+"""
+
+class Team:
+    def __init__(self, name, stadium, city):
+        self.name = name
+        self.stadium = stadium
+        self.city = city
+        
+    def __repr__(self):
+        return f"{self.name},{self.stadium},{self.city}"
+    
+def parseTeamData(data):
+    data1 = data.split(":")
+    club = data1[0]
+    data2 = data1[1].split("-")
+    stadium = data2[0]
+    area = data2[1]
+    
+    return Team(club, stadium, area)
+
+def load_teams_fr_file():
+    try:
+        teams = []
+        my_file = open ("epl.txt", "r")
+        for line in my_file:
+            teams.append(parseTeamData(line))
+    except FileNotFoundError:
+        print("Please save the file in the project folder!")
+    finally:
+        my_file.close()
